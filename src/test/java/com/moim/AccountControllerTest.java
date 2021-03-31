4cee9801dcfb09612a7542ea0cc824737c28801a
mdjuniooor <mmdsds@u.sogang.ac.kr>
2021-03-31 21:23:33 +0900

    Apply Profiles In Test

--- src/test/java/com/moim/AccountControllerTest.java
+++ src/test/java/com/moim/AccountControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,18 +17,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 // 실제 서블릿 컨테이너가 뜨지 않음. mock객체 생성하여 테스트
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("local")
 class AccountControllerTest {
 
     @Autowired private MockMvc mockMvc;
 
-    @DisplayName("회원가입화면")
+    @DisplayName("회원 가입 화면 보이는지 테스트")
     @Test
     void signUpForm() throws Exception {
         mockMvc.perform(get("/sign-up"))
-                .andExpect(status().isOk())
                 .andDo(print())
+                .andExpect(status().isOk())
                 .andExpect(view().name("account/sign-up"))
-                .andExpect(model().attributeExists("signUpForm "));
-
+                .andExpect(model().attributeExists("signUpForm"));
     }
 }
