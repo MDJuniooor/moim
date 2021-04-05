28f95b2b8ed59a397ec59badb0b10462746b6cff
mdjuniooor <mmdsds@u.sogang.ac.kr>
2021-04-05 22:43:30 +0900

    Edit Profile Form

--- src/main/java/com/moim/settings/Profile.java
+++ src/main/java/com/moim/settings/Profile.java
@@ -2,8 +2,10 @@ package com.moim.settings;
 
 import com.moim.domain.Account;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class Profile {
 
     private String bio;
